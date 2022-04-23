fd406366281cf13ed701a0aa12efa1aeeceb7e31
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-04-23 14:30:57 +0300

    impl: minting

--- contracts/Food.sol
+++ contracts/Food.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Market.sol";
 
-contract Food is ERC20 {
-    constructor() ERC20("Food", "FOOD") {}
+contract Food is Context, ERC20 {
+    address _creator;
+
+    constructor() ERC20("Food", "FOOD") {
+        _creator = msg.sender;
+    }
+
+    function creator() external view returns (address) {
+        return _creator;
+    }
+
+    function mint(address to, uint256 amount) external virtual {
+        require(_msgSender() == _creator,
+            "Only creator can mint new tokens"
+        );
+        _mint(to, amount);
+    }
 }
--- test/Food.test.js
+++ test/Food.test.js
@@ -1,10 +1,37 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Food Token', () => {
-    it('Should set correct name and symbol', async () => {
-        const FoodTokenFactory = await ethers.getContractFactory('Food');
-        const FoodToken = await FoodTokenFactory.deploy();
-        expect(await FoodToken.name()).to.equal('Food');
-        expect(await FoodToken.symbol()).to.equal('FOOD');
+describe('Food Token', async () => {
+    let deployer, randomAcc;
+    let FoodTokenFactory, FoodToken;
+
+    beforeEach(async () => {
+        [deployer, randomAcc] = await ethers.getSigners();
+        FoodTokenFactory = await ethers.getContractFactory('Food');
+        FoodToken = await FoodTokenFactory.deploy();
     });
+
+    describe('Upon deployment', () => {
+        it('Should set correct name and symbol', async () => {
+            expect(await FoodToken.name()).to.equal('Food');
+            expect(await FoodToken.symbol()).to.equal('FOOD');
+        });
+
+        it('Should save creator', async () => {
+            expect(await FoodToken.creator()).to.equal(deployer.address);
+        });
+    });
+
+    describe('Minting', () => {
+        it('Should pass when creator', async () => {
+            await FoodToken.connect(deployer).mint(deployer.address, 1);
+            expect(await FoodToken.balanceOf(deployer.address)).to.equal(1);
+        });
+
+        it('Should revert when non-creator', () => {
+            expect(FoodToken.connect(randomAcc).mint(randomAcc.address, 1))
+                .to.be.revertedWith('Only creator can mint new tokens');
+        });
+    });
+
 });
