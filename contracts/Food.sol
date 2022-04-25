5cd7c52f266518e2248eca609814a78202816855
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-04-25 18:57:58 +0300

    add: _pet contract prop to food token

--- contracts/Food.sol
+++ contracts/Food.sol
@@ -7,6 +7,7 @@ import "./Pet.sol";
 
 contract Food is ERC20 {
     Market private _market;
+    Pet private _pet;
 
     modifier onlyMarket() {
         require(
@@ -18,10 +19,15 @@ contract Food is ERC20 {
 
     constructor() ERC20("Food", "FOOD") {
         _market = Market(_msgSender());
+        _pet = _market.pet();
     }
 
     function market() public view returns (Market) {
         return _market;
+    }
+
+    function pet() public view returns (Pet) {
+        return _pet;
     }
 
     function mint(address to, uint256 amount) external onlyMarket {
@@ -31,6 +37,6 @@ contract Food is ERC20 {
     function feedPet(uint256 petId, uint256 amount) external {
         require(amount >= 0.01 ether, "Cannot feed pet with less than 1 FOOD");
         _burn(_msgSender(), amount);
-        _market.pet().feed(petId);
+        _pet.feed(petId);
     }
 }
