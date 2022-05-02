c465b8a63a4d95f8672717b9f6f0e142ef778039
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-05-02 12:20:50 +0300

    initial commit

---
+++ app/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+});
+
+
+export default store;
