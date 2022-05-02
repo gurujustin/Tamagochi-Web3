28a2af71023bff95f9ac9cd8bea5540fe34fdcab
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-05-02 23:40:47 +0300

    create: deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 
 /**
@@ -5,4 +6,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.13",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/0dc62080d95a458fbcd7bd0e28a1a95d`,
+      accounts: [process.env.ROPSTEN_KEY]
+    }
+  }
 };
