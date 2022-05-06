d899707f90247b4fc144324b3637871d73064f44
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-05-07 01:10:40 +0300

    fix: provider bug

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -2,12 +2,16 @@ import { createStore } from 'vuex';
 import { ethers } from 'ethers';
 import router from '../router';
 import { create } from "ipfs-http-client";
+import MarketData from "../contracts/Market";
+import FoodData from "../contracts/Food";
+import PetData from "../contracts/Pet";
+
+const provider = window.ethereum ? new ethers.providers.Web3Provider(window.ethereum) : null;
 
 const store = createStore({
   state: {
     userAddress: null,
     chainId: null,
-    provider: window.ethereum ? new ethers.providers.Web3Provider(window.ethereum) : null,
     ipfs: create("https://ipfs.infura.io:5001/api/v0"),
     MarketContract: null,
     FoodContract: null,
@@ -33,7 +37,6 @@ const store = createStore({
   getters: {
     userAddress: (state) => state.userAddress,
     chainId: (state) => state.chainId,
-    provider: (state) => state.provider,
     ipfs: (state) => state.ipfs,
     MarketContract: (state) => state.MarketContract,
     FoodContract: (state) => state.FoodContract,
@@ -43,12 +46,25 @@ const store = createStore({
     async setupMetamask({ getters, commit }) {
       if (!window.ethereum) return;
       if (getters.userAddress?.length === 42 && getters.chainId === 3) return;
-      const connected = (await store.getters.provider?.listAccounts()).length > 0;
+      const connected = (await provider?.listAccounts()).length > 0;
       if (!connected) return;
-      await getters.provider.send("eth_requestAccounts", []);
-      const signer = getters.provider.getSigner();
+
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+
       commit("userAddress", await signer.getAddress());
       commit("chainId", parseInt(await window.ethereum.request({ method: 'eth_chainId' })));
+
+      const MarketContract = new ethers.Contract(MarketData.address, MarketData.abi, signer);
+      const FoodContract = new ethers.Contract(await MarketContract.token(), FoodData.abi, signer);
+      const PetContract = new ethers.Contract(await MarketContract.pet(), PetData.abi, signer);
+
+      commit('MarketContract', MarketContract);
+      commit('FoodContract', FoodContract);
+      commit('PetContract', PetContract);
+
+      console.log(PetContract.address);
+
       window.ethereum.on("chainChanged", async (_chainId) => {
         const id = parseInt(_chainId, 16);
         commit('chainId', id);
@@ -62,11 +78,6 @@ const store = createStore({
         if (!_accounts[0]) location.reload();
         else router.push('/');
       });
-    },
-    setupContracts({ commit }, contracts) {
-      commit('MarketContract', contracts.MarketContract);
-      commit('FoodContract', contracts.FoodContract);
-      commit('PetContract', contracts.PetContract);
     }
   }
 });
